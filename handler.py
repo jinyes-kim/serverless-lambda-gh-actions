5958bb58ccf5c897b55e23b45422c69b303833c2
jinyes-kim <jinung@kakao.com>
2022-04-04 15:07:08 +0900

    Test for dependencies

--- handler.py
+++ handler.py
@@ -1,8 +1,12 @@
 import json
+import pandas as pd
 
 
 def greeting(event, context):
+    dummy = pd.DataFrame({"name": ['jinung'], "age": [27]})
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from lambda, This API Deployed from serverless framework & Github Actions!!!')
+        'body': json.dumps({"greeting": 'Hello from lambda, This API Deployed from serverless framework & Github Actions!!!', 
+        "data": dummy})
     }
